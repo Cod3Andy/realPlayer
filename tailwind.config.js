abfc12faa8db90c268c62cd9bc6b3828094ffff8
Cod3Andy <derangedjk75@gmail.com>
2023-07-20 22:02:09 +0300

    working progress

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+      },
+      colors: {
+        primary: {
+          100: '#FF6363',
+          200: '#FF9696',
+        },
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Raleway']
+      },
+    },
+  },
+  plugins: [],
+}
+
