2520b62448dbf09adf75bf6992e57cd69983fb8f
Cod3Andy <derangedjk75@gmail.com>
2023-07-22 19:11:07 +0300

    nothing works, but I got the spirit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-    let audio: HTMLAudioElement
     let tracks: File[] = [];
     let songIndex: number = 0;
+    let audio: HTMLAudioElement;
     let isPlaying: boolean = false;
+    let isLooped: boolean = false;
+    let isLiked: boolean = false;
     let volume: number = 0.5;
-    let timeline = 0;
     let duration: number;
     let totalTimeDisplay = "loading...";
 	let currTimeDisplay = "0:00:00";
-    let sliderValue: number = 0;
-    let ended: boolean = false;
-    $: progress = 0;
+    let progress: number = 0;
 
     function handleFileInput(event) {
         const files = event.target.files;
@@ -22,73 +21,65 @@
         audio.pause();
         isPlaying = !isPlaying;
         if(isPlaying === false) {
-            isPlaying = true
+            isPlaying = true;
         }
         audio = new Audio(URL.createObjectURL(track));
         isPlaying? audio.play() : audio.pause();
         audio.play();
     }
     function playPauseAudio() {
+        audio === null ? null : 
         isPlaying = !isPlaying;
         isPlaying? audio.play() : audio.pause();
-        console.log(isPlaying);
     }
-    function timeChange(sliderValue) {
-        timeline = sliderValue
-    }
-    function timeControl(sliderValue) {
-        duration = audio.duration
-        audio.currentTime = sliderValue
-    };
     function volumeControl() {
         if (audio) {
-            audio.volume = volume
+            audio.volume = volume;
         }
     }
     function muteAudio() {
         audio.volume > 0 ? (audio.volume = 0) : (audio.volume = 0.5);
-        volume = audio.volume
+        volume = audio.volume;
     }
     function loopAudio () {
-    audio.loop === true ? audio.loop = false : audio.loop = true;
+        isLooped = !isLooped;
+        audio.loop === true ? audio.loop = false : audio.loop = true;
     }
     function prevAudio () {
-        songIndex--
+        songIndex--;
         if(songIndex < 0) {
-            songIndex = tracks.length - 1
+            songIndex = tracks.length - 1;
         }
-        if(isPlaying === false) {
-            isPlaying = true
+        if(!isPlaying) {
+            isPlaying = true;
         }
-        audio.pause()
+        audio.pause();
         audio = new Audio(URL.createObjectURL(tracks[songIndex]));
-        audio.play()
+        audio.play();
     }
     function nextAudio () {
-        songIndex++
+        songIndex++;
         if(songIndex > tracks.length - 1) {
-            songIndex = 0
+            songIndex = 0;
         }
         audio.pause()
-        if(isPlaying === false) {
-            isPlaying = true
+        if(!isPlaying) {
+            isPlaying = true;
         }
         audio = new Audio(URL.createObjectURL(tracks[songIndex]));
-        audio.play()
+        audio.play();
     }
     function setProgress() {
         if (audio) {
-            audio.currentTime = progress / 100 * audio.duration 
+            audio.currentTime = progress / 100 * audio.duration;
         }
     }
-	// $:tracks[songIndex].onloadedmetadata = () => {
-	// 	duration = audio.duration;
-	// 	updateProgress();
-	// }
+	$: isPlaying? updateProgress() : null;
 
     function updateProgress() {
+		duration = audio.duration;
         progress = audio.currentTime * (100 / duration);
-		
+
 		let currHrs = Math.floor((audio.currentTime / 60) / 60);
 		let currMins = Math.floor(audio.currentTime / 60);
 		let currSecs = Math.floor(audio.currentTime - currMins * 60);
@@ -109,33 +100,21 @@
 			nextAudio();
 		}
     }
-    // function format(seconds) {
-    //     if (audio) {
-    //         const minutes = Math.floor(seconds / 60)
-    //         seconds = Math.floor(seconds % 60)
-    //             if(seconds < 10) {
-    //                 seconds = '0' + seconds
-    //             }
-    //         return `${minutes}:${seconds}`
-    //     } else {
-    //         return '00:00'
-    //     }
-    // }
-    function getPercentage(currentTime) {
-        const percentage = (Math.floor(currentTime) / duration) * 100 + "%"
-        return percentage
+    function likeAudio () {
+        isLiked = !isLiked;
     }
-    // let currentTime = `${}`
 </script>	
 <player class="player">
 <img src="img/gramophone.png" alt="" class="gramophone" />
     <div class="music-container {isPlaying? 'play': ''}">
         <div class="music-info">
-            <p class=" font-mono font-bold text-xl text-primary-100">{isPlaying? tracks[songIndex].name : ''}</p>
-            <div class="current-time">{currTimeDisplay}</div><div class="total-duration">{totalTimeDisplay}</div>
+            <p class=" font-mono font-bold text-xl text-primary-100" id="songTitle">{isPlaying? tracks[songIndex].name  : ''}</p>
+            <div class="flex justify-between">
+                <span id="progress-time">{currTimeDisplay}</span>
+                <span id="track-duration">{totalTimeDisplay}</span>
+            </div>
             <div class="progress-container">
                 <div class="progress"></div>
-                <!-- <input type="range" class="seek-slider" min={0} max={duration} bind:value={sliderValue} on:input={() => timeChange(sliderValue)}> -->
                 <input type="range" class="seek-slider" min={0} max={100} step={1} bind:value={progress} on:change={setProgress}>
                 <seek-slider />
             </div>
@@ -155,13 +134,13 @@
                 <i class="fas fa-forward"></i>
             </button> 
             <button id="loop" class="action-btn" on:click={loopAudio}>
-                <i class="fas fa-arrows-rotate"></i>
+                <i class="fas {isLooped === true ? 'fa-arrows-rotate text-red-600 scale-150 ' : 'fa-arrows-rotate'}"></i>
             </button>
-            <div class="Volume">
+            <div class="Volume group">
                 <button id="mute" class="action-btn" on:click={muteAudio}>
                     <i class="fas {volume > 0.7 && volume > 0 ? 'fa-volume-high' : 'fa-volume-low'} {volume === 0 ? 'fa-volume-xmark' : ''}"></i>
                 </button>
-                <div class="slider" style="width: {volume * 100}%">
+                <div class="slider hidden group-hover:block" style="width: {volume * 100}%">
                     <input type="range" min={0} max={1} step={0.01} bind:value={volume} on:change={volumeControl}>
                 </div>
             </div>
@@ -170,8 +149,12 @@
         </div>
         <div class="playlist">
             {#each tracks as track }
-                <div class="playsongs" on:pointerdown={()=>{getTrack(track)}}>
-                    {track.name}
+                <div class="playlist-elements">
+                    <div class="playsongs" on:pointerdown={()=>{getTrack(track)}}>
+                        {track.name}
+                    </div>
+                    <p></p>
+                    <button class="fa-{isLiked? 'solid' : 'regular'} fa-heart like-btn {isLiked? 'text-4xl' : 'text-3xl'}" on:click={likeAudio}></button>
                 </div>
             {/each}
         </div>
